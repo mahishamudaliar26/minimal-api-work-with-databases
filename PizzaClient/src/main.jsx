ef7ffe92637808670c1eec98b16e540aa4b12400
Mahisha Mudaliar <150228331+mahishamudaliar26@users.noreply.github.com>
2024-02-18 19:36:54 +0000

    first commit

---
+++ PizzaClient/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Pizza from './components/Pizza.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Pizza/>
+  </React.StrictMode>,
+)
